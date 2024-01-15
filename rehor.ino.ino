d60fef620ced4770ac9384ce5061c9ba46a04cd5
Liduskaaa <155913372+Liduskaaa@users.noreply.github.com>
2024-01-15 18:51:40 +0100

    rehor.ino.ino
    aktuální 15.1.2024

--- rehor.ino.ino
+++ rehor.ino.ino
@@ -1,36 +1,47 @@
-t#define I2C_SDA A4 // definice analogových pinů pro display
+#define I2C_SDA A4
 #define I2C_SCL A5
-#include "LiquidCrystal_I2C.h" // knihovna pro display s převodníkem
+#include "LiquidCrystal_I2C.h"
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); // definice displaye
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-const int tlacitka[] = {3,2,5,4}; // vytvoření konstantního pole s čísly pinů tlačítek
-int stisk = 0; // nastvanení proměnných
+const int tlacitka[] = {3,2,5,4};
+int stisk_1 = 0;
+int stisk_2 = 0;
+int stisk_3 = 0;
 int vlhkost = 0;
 int tlac = 0;
+int cilova = 0;
+int frek = 0;
 const int Rele = 6;
 
-void setup() { // spouštěcí funkce
-  Serial.begin(9600); // nastavený rychlosti komunikace
-  Serial.println(""); // výpis do seriálovéo terminálu
+void setup() {
+  Serial.begin(9600);
+    lcd.begin();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Rehor sees you");
+  Serial.println("");
   Serial.println("NOVÉ TESTOVÁNÍ");
-  lcd.begin(); // spuštění displaye
-  lcd.clear(); // vyčištění displaye
-  lcd.setCursor(0, 0); // nastavení kurzoru na 0. sloupec a 0. řádek
-  lcd.print("aaa");
   for(int x=0; x<4; x++)
   {
     // zapojení tlačítek jako vstup s pull-up odporem
     pinMode(tlacitka[x], INPUT_PULLUP);
-  } 
+  }
 
   pinMode(Rele, OUTPUT);
+  delay(3000);
+  lcd.clear();
 }
 
 void loop() {
+  vlhkost = analogRead(A0);
   Serial.println("chacha");
+  lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Joooo :)");
+
+  lcd.print("Hum: " + (String) vlhkost + "/" + (String) cilova);
+  lcd.setCursor(0, 1);
+  lcd.print("Frequency: ");
 
   digitalWrite(Rele, LOW);
   delay(2000);
@@ -43,20 +54,39 @@ void loop() {
   Serial.println(vlhkost);
   delay(1000);
 
-  for(int x=0; x<4; x++)
-  {
-    //načtení stavu tlačítka a uložení do proměnné stisk
-    stisk = digitalRead(tlacitka[x]);
-
-    // pokud je tlačítko stisknuto
-    // vykonej následující
-    if (stisk == LOW) {    
-      // vytiskni pres sériovou linku číslo tlačítka a zprávu
-      tlac = x+1;
-      Serial.print(tlac);
-      Serial.println(" stisknuto");
-      // vyčkej 500 ms kvůli zbytečnému množství tisknutých znaků
-      delay(500);
-    }
+  while (stisk_1 == HIGH) {
+    stisk_1 = digitalRead(tlacitka[0]);
+    stisk_2 = digitalRead(tlacitka[0]);
   }
+
+  if (stisk_1 == LOW) { // vytiskni pres sériovou linku číslo tlačítka a zprávu
+    tlac = 1;
+    Serial.print(tlac);
+    Serial.println(" stisknuto");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Change humidity:");
+    lcd.setCursor(0, 1);
+    lcd.print("<   " + (String) cilova + "   >");
+    stisk_3 = digitalRead(tlacitka[2]);
+    while (stisk_3 == LOW) {
+      cilova = cilova - 5;
+      delay(500);
+      lcd.setCursor(0, 1);
+      lcd.print("<   " + (String) cilova + "   >");
+      stisk_3 = digitalRead(tlacitka[2]);
+    }
+    delay(500);
+  }
+/*
+  else if (stisk_2 == LOW) {
+    tlac = 2;
+    Serial.print(tlac);
+    Serial.println(" stisknuto");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Change freq:");
+    lcd.setCursor(0, 1);
+    lcd.print("<   " + (String) frek + "   >");
+  }*/
 }
